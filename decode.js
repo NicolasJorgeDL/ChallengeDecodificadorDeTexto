9945fed6babaf14099901232b585bfc2c46687b4
Nicolas Jorge <nicolasjorgedl@hotmail.com>
2024-08-14 19:49:54 -0300

    modificando como o resultado é apresentado e removendo cifra Vigenere

--- decode.js
+++ decode.js
@@ -1,100 +1,88 @@
-
-function TextoVazio(texto){
-    if (texto == ""){
-        document.getElementById("texto_resultante").value = "Digite o texto para ser criptografado ou descriptografado";
-        return true;
-    }
-    return false;
-}
-
-function TextoValido(texto){
-    if (TextoVazio(texto)){
-        return false;
-    }
-    if (/[^a-z\s]/.test(texto)){
-        document.getElementById("texto_resultante").value = "O texto está invalido, por favor altere o texto para apenas conter letras minúsculas e sem acento.";
-        return false;
-    }
+function TextoVazio(texto) {
+  if (texto == "") {
+    document.getElementsByClassName("resultado__card")[0].innerHTML =
+      '<img class="resultado__img" src="/assets/Imagen-nenhuma-mensagem.svg"><h3 class="resultado__card__destaque">Nenhuma mensagem encontrada</h3><p class="resultado__card__texto">Digite um texto que você deseja criptografar ou descriptografar.</p>';
     return true;
+  }
+  return false;
 }
 
-function CifraVigenere(texto, criptografar){
-    const chave = "laranjabananamaca";
-    var chaveExpandida = chave.repeat(Math.ceil(texto.length / chave.length));
-    chaveExpandida = chaveExpandida.substring(0, texto.length);
-
-    if (criptografar){      
-        return texto.split('').map((char, i) => {
-            if (char >= 'a' && char <= 'z') { 
-              const code = char.charCodeAt(0);
-              const deslocamento = chaveExpandida[i].charCodeAt(0) - 97; 
-        
-              return String.fromCharCode(((code - 97 + deslocamento) % 26) + 97);
-            }
-            return char;
-          }).join('');
-    }else {
-        return texto.split('').map((char, i) => {
-            if (char >= 'a' && char <= 'z') {
-              const code = char.charCodeAt(0);
-              const deslocamento = chaveExpandida[i].charCodeAt(0) - 97; 
-        
-              return String.fromCharCode(((code - 97 - deslocamento + 26) % 26) + 97);
-            }
-
-            return char;
-          }).join('');
-    }
+function TextoValido(texto) {
+  if (TextoVazio(texto)) {
+    return false;
+  }
+  if (/[^a-z\s]/.test(texto)) {
+    document.getElementsByClassName("resultado__card")[0].innerHTML =
+      '<img class="resultado__img" src="/assets/Imagen-nenhuma-mensagem.svg"><h3 class="resultado__card__destaque">Mensagem invalida</h3><p class="resultado__card__texto">O texto está invalido, por favor altere o texto para apenas conter letras minúsculas e sem acento.</p>';
+    return false;
+  }
+  return true;
 }
 
-
-function CriptografiaSecreta(texto, criptografar){
-    
-    if (criptografar){
-        return texto.split('').map((char, i) =>{
-            if (char == 'e'){
-                return 'enter';
-            }
-            if (char == 'i'){
-                return 'imes';
-            }
-            if (char == 'a'){
-                return 'ai';
-            }
-            if (char == 'o'){
-                return 'ober';
-            }
-            if (char == 'u'){
-                return 'ufat';
-            }
-            return char;
-        }).join('');   
-    }else {
-        var textoTraduzido = texto.replace(/enter/g,"e");
-        textoTraduzido = textoTraduzido.replace(/imes/g,"i");
-        textoTraduzido = textoTraduzido.replace(/ai/g,"a");
-        textoTraduzido = textoTraduzido.replace(/ober/g,"o");
-        textoTraduzido = textoTraduzido.replace(/ufat/g,"u");
-        return textoTraduzido;
-    }
+function CriptografiaSecreta(texto, criptografar) {
+  if (criptografar) {
+    return texto
+      .split("")
+      .map((char, i) => {
+        if (char == "e") {
+          return "enter";
+        }
+        if (char == "i") {
+          return "imes";
+        }
+        if (char == "a") {
+          return "ai";
+        }
+        if (char == "o") {
+          return "ober";
+        }
+        if (char == "u") {
+          return "ufat";
+        }
+        return char;
+      })
+      .join("");
+  } else {
+    var textoTraduzido = texto.replace(/enter/g, "e");
+    textoTraduzido = textoTraduzido.replace(/imes/g, "i");
+    textoTraduzido = textoTraduzido.replace(/ai/g, "a");
+    textoTraduzido = textoTraduzido.replace(/ober/g, "o");
+    textoTraduzido = textoTraduzido.replace(/ufat/g, "u");
+    return textoTraduzido;
+  }
 }
 
-function Criptografar(){
-    var texto = document.getElementById("texto_entrada").value;
-    if (!TextoValido(texto)){
-        return false;
-    }
-
-    let textoCriptografado = CriptografiaSecreta(texto, true);
-
-    document.getElementById("texto_resultante").value = textoCriptografado;
+function Criptografar() {
+  var texto = document.getElementsByClassName("texto_entrada")[0].value;
+  if (!TextoValido(texto)) {
+    return false;
+  }
+  let textoCriptografado = CriptografiaSecreta(texto, true);
+  document.getElementsByClassName("resultado__card")[0].innerHTML =
+    '<textarea class="resultado__texto" wrap="hard" readonly>' +
+    textoCriptografado +
+    '</textarea><button class= "resultado__button_copiar" onclick="CopiarTextoResultado()">Copiar</button>';
 }
 
-function Descriptografar(){
-    var texto = document.getElementById("texto_entrada").value;
-    if (!TextoVazio(texto)){
-        console.log(texto)
-        document.getElementById("texto_resultante").value = CriptografiaSecreta(texto, false);
-    }
+function Descriptografar() {
+  var texto = document.getElementsByClassName("texto_entrada")[0].value;
+  if (!TextoValido(texto)) {
+    return false;
+  }
+  let textoDescriptografado = CriptografiaSecreta(texto, false);
+  document.getElementsByClassName("resultado__card")[0].innerHTML =
+    '<textarea class="resultado__texto" wrap="hard" readonly>' +
+    textoDescriptografado +
+    '</textarea><button class= "resultado__button_copiar" onclick="CopiarTextoResultado()">Copiar</button>';
 }
 
+// O comando execCommand foi descontinuado, então ele vai ser apenas utilizado quando não tiver a permissão para acessar o clipboard, permitindo funcionar na maioria dos navegadores
+function CopiarTextoResultado() {
+  let texto = document.getElementsByClassName("resultado__texto")[0].value;
+  if (!navigator.clipboard) {
+    document.getElementsByClassName("resultado__texto")[0].select();
+    document.execCommand("copy");
+  } else {
+    navigator.clipboard.writeText(texto);
+  }
+}
